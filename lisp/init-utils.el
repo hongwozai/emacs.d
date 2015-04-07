8ee27df71b1f22922f39dfa69aa3b78731944fa8
hongwozai <hongwozai@163.com>
2015-04-07 22:55:58 +0800

    add little changs

--- lisp/init-utils.el
+++ lisp/init-utils.el
@@ -12,5 +12,28 @@
 (menu-bar-mode -1)
 
 ;; mode line
+(defvar hidden-minor-modes ; example, write your own list of hidden
+  '(abbrev-mode            ; minor modes
+    auto-fill-function
+    flycheck-mode
+    flyspell-mode
+    paredit-mode
+    hs-minor-mode
+    eldoc-mode
+    helm-mode
+    company-search-mode
+    company-mode
+    undo-tree-mode
+    yas-minor-mode
+    smooth-scroll-mode))
+
+(defun purge-minor-modes ()
+  (interactive)
+  (dolist (x hidden-minor-modes nil)
+    (let ((trg (cdr (assoc x minor-mode-alist))))
+      (when trg
+        (setcar trg "")))))
+
+(add-hook 'after-change-major-mode-hook 'purge-minor-modes)
 
 (provide 'init-utils)
