955b49360be18c748859e2b241976082072a5c09
hongwozai <hongwozai@163.com>
2015-07-27 12:58:32 +0800

    添加键位
    next-error, previous-error收到搜索的影响太多，需要单独添加flycheck的按键

--- lisp/init-evil.el
+++ lisp/init-evil.el
@@ -61,6 +61,8 @@
   "ep"  'previous-error
   "fl"  'flycheck-list-errors
   "ff"  'ffip
+  "fn"  'flycheck-next-error
+  "fp"  'flycheck-previous-error
   "gc"  'ggtags-create-tags
   "gd"  'ggtags-find-definition
   "gt"  'ggtags-find-tag-dwim
