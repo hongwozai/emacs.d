17c9b885c0d76a87d3d0b7c31fc8e8a13d2af3c6
hongwozai <hongwozai@163.com>
2015-08-28 07:30:21 +0800

    配置web-mode与php

--- lisp/init-web.el
+++ lisp/init-web.el
@@ -1,10 +1,11 @@
 ;;; php
 (require-package 'php-mode)
-(require 'php-mode)
+
 ;;; web mode
 (require-package 'web-mode)
 (add-to-list 'auto-mode-alist '("\\.phtml\\'" . web-mode))
-(add-to-list 'auto-mode-alist '("\\.tpl\\.php\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.tpl\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.php\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.[agj]sp\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.as[cp]x\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.erb\\'" . web-mode))
