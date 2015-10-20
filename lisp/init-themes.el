ff174adbfe0b7b4d20a7f2893c715669053dbe29
hongwozai <hongwozai@163.com>
2015-10-20 20:00:27 +0800

    修改mode-line的配色

--- lisp/init-themes.el
+++ lisp/init-themes.el
@@ -1,7 +1,21 @@
 ;; color theme
 (require-package 'zenburn-theme)
 (load-theme 'zenburn t)
-(set-face-foreground 'mode-line "#ffffff")
-(set-face-background 'mode-line "#657b83")
+
+;;; mode-line
+(defvar hong/mode-line-normal "#657b83")
+(defvar hong/mode-line-inactive "DimGrey")
+
+(custom-theme-set-faces
+ 'zenburn
+ `(mode-line
+   ((t (:box (:line-width 1 :color ,hong/mode-line-normal :style nil)
+             :foreground "#ffffff" :background ,hong/mode-line-normal
+             :overline ,hong/mode-line-normal :inverse-video nil))))
+ `(mode-line-inactive
+   ((t (:box (:line-width 1 :color "#657b83" :style nil)
+             :foreground "#ffffff" :background ,hong/mode-line-inactive
+             :overline ,hong/mode-line-inactive :inverse-video nil))))
+ )
 
 (provide 'init-themes)
