98eff4aba590972536161b575f938c3466f6b5e4
hongwozai <hongwozai@163.com>
2015-11-29 16:04:26 +0800

    忘记了elscreen

--- lisp/init-evil.el
+++ lisp/init-evil.el
@@ -54,6 +54,7 @@
 
 ;;; elscreen
 ;;; elscreen needs mode-line-format
+(require-package 'elscreen)
 (setq elscreen-display-tab nil)
 (setq elscreen-tab-display-control nil)
 (setq elscreen-tab-display-kill-screen nil)
