4b3c3a61ce42f8d591f92752261375f6fed9c11f
hongwozai <hongwozai@163.com>
2015-12-15 21:04:25 +0800

    change

--- lisp/init-web.el
+++ lisp/init-web.el
@@ -18,7 +18,7 @@
 
 (setq web-mode-markup-indent-offset 2)
 (setq web-mode-css-indent-offset    2)
-(setq web-mode-code-indent-offset   2)
+(setq web-mode-code-indent-offset   4)
 
 (setq web-mode-enable-auto-pairing  t)
 (setq web-mode-enable-css-colorization t)
