05e9eb82c63e6ff66505279a7a57cece3b1568bf
hongwozai <hongwozai@163.com>
2015-12-18 15:09:51 +0800

    去掉注释的背景颜色，因为终端下太难看

--- lisp/init-themes.el
+++ lisp/init-themes.el
@@ -1,6 +1,8 @@
 ;; color theme
 (require-package 'spacemacs-theme)
 
+(setq spacemacs-theme-comment-bg nil)
+
 ;;; only graphic color
 (flet ((display-graphic-p (&optional what) t))
   (load-theme 'spacemacs-dark t))
