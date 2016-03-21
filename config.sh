3eff07417e8cdda442f652bb008f3a83e115d901
hongwozai <hongwozai@163.com>
2016-03-21 13:21:35 +0800

    安装文件添加别名

--- config.sh
+++ config.sh
@@ -12,8 +12,24 @@ if [ -n "$INSIDE_EMACS" ]; then
 fi
 EOF
 }
+
+function em_sudo() {
+    emacsclient -nw -a "" -c /sudo:root@localhost:$1
+}
+
+function em_alias() {
+    cat <<EOF >> $1
+alias em='emacsclient -nw -a "" -c'
+alias emsudo='em_sudo'
+EOF
+}
+
 # ============== control flow ============
 # emacs multi-term ch only zsh
 if [ -f ~/.zshrc ]; then
     emacs_term_chpwd "~/.zshrc"
+fi
+
+if [ -f ~/.bashrc ]; then
+    em_alias "~/.bashrc"
 fi
