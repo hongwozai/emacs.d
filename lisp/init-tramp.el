09a19e8bb6f82d32c4e1f83cfe08e59697fba4d6
hongwozai <hongwozai@163.com>
2016-03-29 18:43:44 +0800

    进一步加快tramp速度

--- lisp/init-tramp.el
+++ lisp/init-tramp.el
@@ -13,7 +13,12 @@ buffer is not visiting a file."
 
 ;;; ssh is faster than scp. scpx or sshx. see Tramp hangs
 (setq tramp-default-method "ssh")
-(setq tramp-verbose 0)
+(setq tramp-verbose 2)
+(setq tramp-connection-timeout 10)
+
+;;; save password
+(setq password-cache t)
+(setq password-cache-expiry 120)
 
 ;;; faster see wiki Tramp hangs #2
 (setq tramp-chunksize 8192)
