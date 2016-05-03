660ae68714f7aac797463c232c2599a428e33a46
lm <lm@localhost.localdomain>
2016-05-03 18:20:46 +0800

    依然是为了保证最小核心，gtk用.dir-locals.el文件代替

--- lisp/init-cc-mode.el
+++ lisp/init-cc-mode.el
@@ -3,24 +3,6 @@
 (autoload 'doxygen-insert-function-comment "doxygen" "insert comment for the function at point" t)
 (autoload 'doxygen-insert-file-comment "doxygen" "insert comment for file" t)
 
-;;; ===================== gtk config ===========================
-(defun hong/gtk-headers ()
-  (let* ((cmd "pkg-config --cflags gtk+-3.0")
-         (gtk (split-string
-               (shell-command-to-string cmd))))
-    (mapcar #'(lambda (x) (substring x 2))
-            (remove-if #'(lambda (x) (not (eql (elt x 1) ?I))) gtk))))
-
-(defun hong/my-gtk-config ()
-  (interactive)
-  (setq-local flycheck-clang-include-path
-              (append flycheck-clang-include-path (hong/gtk-headers)))
-  (setq-local company-clang-arguments
-              (append company-clang-arguments
-                      (mapcar #'(lambda (x) (concat "-I" x)) (hong/gtk-headers))))
-  (setq-local company-c-headers-path-system
-              (append company-c-headers-path-system (hong/gtk-headers)))
-  )
 ;;; ===================== cc mode config ========================
 ;;; include c/c++, java etc.
 (defun cc-common-config ()
@@ -51,12 +33,7 @@
 
 (defun hong/tags-debug-compile-setup (map)
   (define-key map (kbd "C-c C-c") 'project-compile-in-shell)
-  (define-key map (kbd "C-c C-s") 'change-compile-command)
-  (define-key map (kbd "M-,") 'ggtags-prev-mark)
-  (define-key map (kbd "M-.") 'ggtags-find-definition)
-  (evil-define-key 'normal map (kbd "M-.") 'ggtags-find-definition)
-  (define-key map (kbd "C-M-.") 'ggtags-find-other-symbol)
-  (define-key map (kbd "<f6>") 'gdb))
+  (define-key map (kbd "C-c C-s") 'change-compile-command))
 
 (add-hook 'c-mode-common-hook
           (lambda () (cc-common-config)))
