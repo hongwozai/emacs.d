7c566a2da1b10d662a119669c168067fddd94319
lm <lm@hong>
2016-05-06 19:31:23 +0800

    去掉调试的语句

--- lisp/init-themes.el
+++ lisp/init-themes.el
@@ -5,7 +5,6 @@
 ;;; theme change
 (add-hook 'after-init-hook
           (lambda ()
-            (set-face-italic 'font-lock-function-name-face t)
             (set-face-italic 'font-lock-comment-face t)
             (set-face-bold 'font-lock-type-face t)
             (set-face-bold 'font-lock-keyword-face t)
