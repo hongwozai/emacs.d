06d6569f294feca129eb362800c1b2b199117a92
lm <lm@hong>
2016-05-13 20:47:55 +0800

    修复bug

--- lisp/init-template.el
+++ lisp/init-template.el
@@ -1,5 +1,5 @@
 ;;; configure
-;; (setq yas-verbosity 0)
+(setq yas-verbosity 0)
 (setq yas-snippet-dirs '("~/.emacs.d/snippets"))
 (setq yas-prompt-functions '(yas-ido-prompt
                              yas-completing-prompt))
