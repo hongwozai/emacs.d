35eedd849a3402198f067e655e26cf0765f19dac
lm <lm@hong>
2016-05-13 13:15:26 +0800

    调整窗口

--- lisp/init-window.el
+++ lisp/init-window.el
@@ -19,9 +19,24 @@
       (split-window nil nil direct)
       (switch-to-buffer-other-window other-buffer))))
 
-;;; evil key bindings
+;;; key bindings
 (define-key evil-normal-state-map (kbd "C-w SPC") 'hong/window-layout-change)
 (define-key evil-motion-state-map (kbd "C-w SPC") 'hong/window-layout-change)
+
+;;; windows size(above below left right)
+(defun hong-window-resize (dir)
+  (let* ((horiz? (or (eq dir 'right) (eq dir 'left)))
+         (other (window-in-direction (if horiz? 'right 'below)))
+         (plus? (or (eq dir 'right) (eq dir 'below)))
+         (num 2)
+         (amount (if (eq (not other) (not plus?))
+                     num (- num))))
+    (enlarge-window amount horiz?)))
+
+(global-set-key (kbd "C-S-<left>") (lambda () (interactive) (hong-window-resize 'left)))
+(global-set-key (kbd "C-S-<right>") (lambda () (interactive) (hong-window-resize 'right)))
+(global-set-key (kbd "C-S-<up>") (lambda () (interactive) (hong-window-resize 'above)))
+(global-set-key (kbd "C-S-<down>") (lambda () (interactive) (hong-window-resize 'below)))
 
 ;;; workspace
 (setq eyebrowse-wrap-around   t
