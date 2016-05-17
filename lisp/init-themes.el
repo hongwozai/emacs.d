b702ab7219cb84182a5e517c6d3eda3f6cbf8af6
lm <lm@hong>
2016-05-17 11:07:57 +0800

    配色还是改回来，不然显示不和谐

--- lisp/init-themes.el
+++ lisp/init-themes.el
@@ -8,7 +8,7 @@
    'zenburn
    ;; mode-line
    `(mode-line
-     ((t (:box (:line-width 1 :color ,zenburn-bg-1 :style nil)
+     ((t (:box (:line-width 1 :color ,zenburn-bg+1 :style nil)
                :foreground ,zenburn-green+1 :background ,zenburn-bg-1))))
    `(mode-line-inactive
      ((t (:box (:line-width 1 :color ,zenburn-bg-1 :style nil)
