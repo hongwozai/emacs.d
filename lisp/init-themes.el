d793e17402603935115be4d9d63808193e38ad45
lm <lm@hong>
2016-05-26 17:49:29 +0800

    mode-line使用默认的试试

--- lisp/init-themes.el
+++ lisp/init-themes.el
@@ -8,13 +8,6 @@
    'zenburn
    ;; font-lock
    `(font-lock-type-face ((t (:foreground "#90AFD3"))))
-   ;; mode-line
-   `(mode-line
-     ((t (:box (:line-width 1 :color ,zenburn-bg+1 :style nil)
-               :foreground ,zenburn-green+1 :background ,zenburn-bg-1))))
-   `(mode-line-inactive
-     ((t (:box (:line-width 1 :color ,zenburn-bg-1 :style nil)
-               :foreground ,zenburn-fg+1 :background ,zenburn-bg+1))))
    ;; window number
    `(window-numbering-face ((t (:foreground "goldenrod"))))
    ))
