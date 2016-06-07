ca322240d755e21f8b138e7424580ca0051c904d
lm <lm@hong>
2016-06-07 19:27:45 +0800

    添加scheme快捷键2

--- lisp/init-scheme.el
+++ lisp/init-scheme.el
@@ -3,6 +3,12 @@
 (add-hook 'geiser-mode-hook
           (lambda ()
             (evil-leader/set-key-for-mode 'scheme-mode
-              "xe" 'geiser-eval-last-sexp)))
+              "xe" 'geiser-eval-last-sexp)
+            (evil-define-key 'normal scheme-mode-map
+              "M-," 'geiser-pop-symbol-stack
+              "M-." 'geiser-edit-symbol-at-point)
+            (evil-define-key 'insert scheme-mode-map
+              "M-," 'geiser-pop-symbol-stack
+              "M-." 'geiser-edit-symbol-at-point)))
 
 (provide 'init-scheme)
