7533ba9b78110211fc406d28df502537baaa3348
lm <lm@hong>
2016-06-07 19:22:34 +0800

    添加scheme的快捷键

--- lisp/init-scheme.el
+++ lisp/init-scheme.el
@@ -1,3 +1,8 @@
 (require-package 'geiser)
 
+(add-hook 'geiser-mode-hook
+          (lambda ()
+            (evil-leader/set-key-for-mode 'scheme-mode
+              "xe" 'geiser-eval-last-sexp)))
+
 (provide 'init-scheme)
