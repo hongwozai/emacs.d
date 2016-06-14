771c2389aa8054f28fb2e2765db0bf7824b481a3
hongwozai <hongwozai@163.com>
2016-06-14 15:57:12 +0800

    调整颜色

--- lisp/init-themes.el
+++ lisp/init-themes.el
@@ -8,14 +8,17 @@
    'zenburn
    ;; font-lock
    `(font-lock-type-face ((t (:foreground "#90AFD3"))))
+
    ;; window number
    `(window-numbering-face ((t (:foreground "goldenrod"))))
+
    ;; mode-line
    `(mode-line
      ((,class (:foreground ,zenburn-green+1 :background ,zenburn-bg-1))
       (t :inverse-video t)))
    `(mode-line-inactive
      ((t (:foreground ,zenburn-fg+1 :background ,zenburn-bg+1))))
+
    ;; ido
    `(ido-first-match ((t (:foreground ,zenburn-bg-1
                                       :background "#65a7e2"
@@ -23,10 +26,16 @@
    `(ido-only-match ((t (:foreground ,zenburn-bg-1
                                      :background "#65a7e2"
                                      :weight bold))))
+
    ;; linum
    `(linum ((t (:foreground ,zenburn-green :background ,zenburn-bg))))
+
    ;; vertical-border
    `(vertical-border ((t (:foreground ,zenburn-bg+3))))
+
+   ;; rainbow
+   `(rainbow-delimiters-depth-1-face ((t (:foreground ,zenburn-blue-1))))
+
    ;; ivy
    `(ivy-subdir ((t (:background ,zenburn-bg))))
    `(ivy-current-match ((t (:background "#65a7e2" :foreground ,zenburn-bg-1
@@ -35,6 +44,7 @@
    `(ivy-minibuffer-match-face-2 ((t (::background ,zenburn-bg-05))))
    `(ivy-minibuffer-match-face-3 ((t (:background "#7777ff"))))
    `(ivy-minibuffer-match-face-4 ((t (:background "#8a498a"))))
+
    ;; avy
    `(avy-lead-face-0
      ((t (:foreground ,zenburn-bg+1 :background ,zenburn-blue+1 :inverse-video nil))))
