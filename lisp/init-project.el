7114a4545d470ef5d9e75e8d794cdbe0176a6326
hongwozai <hongwozai@163.com>
2016-06-24 20:54:50 +0800

    将gtags认为是工程

--- lisp/init-project.el
+++ lisp/init-project.el
@@ -4,7 +4,7 @@
 ;;; ignore hidden file
 (with-eval-after-load 'find-file-in-project
   (setq ffip-project-file '(".svn" ".git" ".hg" "Makefile"
-                            "makefile" ".dir-locals.el"))
+                            "makefile" ".dir-locals.el" "GTAGS"))
   (add-to-list 'ffip-prune-patterns "*/.*/*"))
 
 (provide 'init-project)
