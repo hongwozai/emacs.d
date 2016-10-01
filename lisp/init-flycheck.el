7be2944db31a3c959a52bb84b72b407d68fd8229
hongwozai <hongwozai@163.com>
2016-10-01 09:24:06 +0800

    javascript不使用另外的checker，仅js2-mode就够用了

--- lisp/init-flycheck.el
+++ lisp/init-flycheck.el
@@ -1,7 +1,9 @@
 ;; ;; flycheck
 (add-hook 'after-init-hook #'global-flycheck-mode)
 
-(setq-default flycheck-disabled-checkers '(emacs-lisp-checkdoc))
+(setq-default flycheck-disabled-checkers
+              '(emacs-lisp-checkdoc javascript-eslint
+                javascript-jshint javascript-gjslint))
 (setq flycheck-idle-change-delay 1)
 (setq flycheck-emacs-lisp-load-path 'inherit)
 
