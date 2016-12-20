58b4d0a9b4ce3636b99240258b662adda8140e13
hongwozai <hongwozai@163.com>
2016-12-20 13:07:18 +0800

    添加延后执行代码

--- lisp/init-web.el
+++ lisp/init-web.el
@@ -37,17 +37,19 @@
               (kbd "za") 'web-mode-fold-or-unfold)))
 
 ;;; php-web checker
-(flycheck-define-checker php-web
-  "A PHP syntax checker using the PHP command line interpreter.
+(eval-after-load "flycheck"
+  '(progn
+    (flycheck-define-checker php-web
+      "A PHP syntax checker using the PHP command line interpreter.
 
 See URL `http://php.net/manual/en/features.commandline.php'."
-  :command ("php" "-l" "-d" "error_reporting=E_ALL" "-d" "display_errors=1"
-                  "-d" "log_errors=0" source)
-  :error-patterns
-  ((error line-start (or "Parse" "Fatal" "syntax") " error" (any ":" ",") " "
-          (message) " in " (file-name) " on line " line line-end))
-  :modes (web-mode)
-  :next-checkers ((warning . php-phpmd)
-                  (warning . php-phpcs)))
+      :command ("php" "-l" "-d" "error_reporting=E_ALL" "-d" "display_errors=1"
+                      "-d" "log_errors=0" source)
+      :error-patterns
+      ((error line-start (or "Parse" "Fatal" "syntax") " error" (any ":" ",") " "
+              (message) " in " (file-name) " on line " line line-end))
+      :modes (web-mode)
+      :next-checkers ((warning . php-phpmd)
+                      (warning . php-phpcs)))))
 
 (provide 'init-web)
