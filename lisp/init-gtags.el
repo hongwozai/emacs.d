d12477f41fbeee4ccbe62aeb393d5543c66ca1e2
hongwozai <hongwozai@163.com>
2017-07-04 22:21:45 +0800

    重载counsel-gtags的函数，不再读取输入进行符号判断

--- lisp/init-gtags.el
+++ lisp/init-gtags.el
@@ -5,4 +5,7 @@
 (define-key evil-normal-state-map (kbd "M-.") 'counsel-gtags-find-definition)
 (define-key evil-normal-state-map (kbd "M-,") 'counsel-gtags-pop)
 
+(defun counsel-gtags--read-tag (type)
+  (thing-at-point 'symbol t))
+
 (provide 'init-gtags)
