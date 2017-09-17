3ae85905073be8e5d3ba51295e384a6d0fd01b04
hongwozai <hongwozai@163.com>
2017-09-17 16:52:59 +0800

    修复bug

--- lisp/init-flycheck.el
+++ lisp/init-flycheck.el
@@ -8,7 +8,9 @@
 (setq flycheck-idle-change-delay 1)
 (setq flycheck-emacs-lisp-load-path 'inherit)
 
-(setq flycheck-clang-args "-std=c++11")
+(add-hook 'c++-mode-hook
+          (lambda ()
+            (setq-local flycheck-clang-args "-std=c++11")))
 
 ;;; flycheck pos-tip
 (setq flycheck-pos-tip-timeout -1)
