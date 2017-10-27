333f8dcfe74e5a9095f2f9f9b7b0afe1a3aa754a
lm <hongwozai@163.com>
2017-10-27 14:09:05 +0800

    python中去掉高亮缩进，没有辨识度

--- lisp/init-python.el
+++ lisp/init-python.el
@@ -6,6 +6,7 @@
 ;;; flymake -> flycheck
 (when (require 'flycheck nil t)
   (setq elpy-modules (delq 'elpy-module-flymake elpy-modules))
+  (setq elpy-modules (delq 'elpy-module-highlight-indentation elpy-modules))
   (add-hook 'elpy-mode-hook 'flycheck-mode))
 
 ;;; define key
