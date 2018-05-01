d405f8db6058ec73fc4cc5b55932f61e1526d031
zeya <hongwozai@163.com>
2018-05-01 04:24:11 -0700

    提供字体大小的更改选项，有需要了使用

--- lisp/init-themes.el
+++ lisp/init-themes.el
@@ -1,4 +1,17 @@
 ;;; gruvbox
 (load-theme 'gruvbox t)
 
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(default
+   ((t (:family "Monospace"
+                :foundry "DAMA"
+                :slant normal
+                :weight normal
+                :height 150
+                :width normal)))))
+
 (provide 'init-themes)
