3011b9b2e148312cc5acebda024ac435161294c7
hongwozai <hongwozai@163.com>
2018-05-21 15:31:25 +0000

    添加octave快捷键

--- lisp/init-octave.el
+++ lisp/init-octave.el
@@ -5,4 +5,11 @@
                '("\\.mat$" . octave-mode))
               auto-mode-alist))
 
+(add-hook 'octave-mode-hook
+          (lambda ()
+            (evil-define-key 'normal octave-mode-map
+              (kbd "C-c C-c") 'octave-send-buffer)
+            (evil-define-key 'insert octave-mode-map
+              (kbd "C-c C-c") 'octave-send-buffer)))
+
 (provide 'init-octave)
