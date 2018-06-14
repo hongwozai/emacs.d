66587a50d2d18d81ae7b246b8c610d150ee52e8f
hongwozai <hongwozai@163.com>
2018-06-14 15:38:05 +0000

    配置go语言的godoc与godef，需要事先安装godoc与godef

--- module/module-go.el
+++ module/module-go.el
@@ -4,3 +4,13 @@
 (module-require "go")
 (require-package 'go-mode)
 (require-package 'go-eldoc)
+
+;;-------------------------------------------
+;;; config
+;;-------------------------------------------
+(add-hook 'go-mode-hook #'go-eldoc-setup)
+
+(with-eval-after-load 'go-mode
+  (core/set-key go-mode-map
+    :state 'normal
+    (kbd "M-.") 'godef-jump))
