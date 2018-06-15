0b66739dce0e5c854ad43ee561a6a47e9b077629
zeya <hongwozai@163.com>
2018-06-14 22:40:07 -0700

    设置gdb的窗口不为dedicated

--- module/module-cc.el
+++ module/module-cc.el
@@ -43,6 +43,21 @@
 (add-to-list 'auto-mode-alist '("\\.h\\'" . c++-mode))
 
 ;;-------------------------------------------
+;;; gdb
+;;-------------------------------------------
+;; Force gdb-mi to not dedicate any windows
+(advice-add 'gdb-display-buffer
+            :around (lambda (orig-fun &rest r)
+                      (let ((window (apply orig-fun r)))
+                        (set-window-dedicated-p window nil)
+                        window)))
+
+(advice-add 'gdb-set-window-buffer
+            :around (lambda (orig-fun name &optional ignore-dedicated window)
+                      (funcall orig-fun name ignore-dedicated window)
+                      (set-window-dedicated-p window nil)))
+
+;;-------------------------------------------
 ;;; cquery
 ;;-------------------------------------------
 (require-package 'cquery)
