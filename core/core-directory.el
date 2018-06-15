20046bf10f698cea1e1c192cef636f673e354d01
hongwozai <hongwozai@163.com>
2018-06-15 13:55:17 +0000

    fix bug.
    dired-load-hook是dired+所附带的hook，在此处无法使用

--- core/core-directory.el
+++ core/core-directory.el
@@ -3,31 +3,26 @@
 ;;-------------------------------------------
 (autoload 'dired-jump "dired")
 
-(add-hook 'dired-load-hook
-          (lambda ()
-            ;; diredp
-            ;; (require 'dired+)
-            ;; (diredp-toggle-find-file-reuse-dir 1)
-            (setq dired-recursive-copies   'always)
-            (setq dired-recursive-deletes  'always)
-            (setq dired-isearch-filenames  t)
-            (setq dired-dwim-target        t)
-            (setq dired-listing-switches
-                  "-aluh --time-style=iso")
+(with-eval-after-load 'dired
+  ;; diredp
+  (setq dired-recursive-copies   'always)
+  (setq dired-recursive-deletes  'always)
+  (setq dired-isearch-filenames  t)
+  (setq dired-dwim-target        t)
+  (setq dired-listing-switches
+        "-aluh --time-style=iso")
 
-            ;; key
-            (core/set-key dired-mode-map
-              :state 'native
-              (kbd "M-o") 'dired-omit-mode
-              [mouse-2]   'dired-find-file)
-            (core/set-key dired-mode-map
-              :state 'normal
-              "j" 'dired-next-line
-              "k" 'dired-previous-line
-              "n" 'evil-search-next
-              "N" 'evil-search-previous)
-            )
-          t)
+  ;; key
+  (core/set-key dired-mode-map
+    :state 'native
+    (kbd "M-o") 'dired-omit-mode
+    [mouse-2]   'dired-find-file)
+  (core/set-key dired-mode-map
+    :state 'normal
+    "j" 'dired-next-line
+    "k" 'dired-previous-line
+    "n" 'evil-search-next
+    "N" 'evil-search-previous))
 
 (add-hook 'dired-mode-hook
           (lambda ()
