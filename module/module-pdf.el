b53c04a7bc91a9f4873ee384cd6254861bec2991
zeya <hongwozai@163.com>
2018-06-22 09:37:30 -0700

    pdf模块

---
+++ module/module-pdf.el
@@ -0,0 +1,31 @@
+;;-------------------------------------------
+;;; pdf tools (need run pdf-tools-install)
+;;-------------------------------------------
+(module-require-manual)
+(require-package 'pdf-tools)
+
+;;-------------------------------------------
+;;; install pdf tools
+;;-------------------------------------------
+(with-eval-after-load "pdf-tools"
+  (evil-set-initial-state 'pdf-view-mode 'normal)
+  (core/set-key pdf-view-mode-map
+    :state '(normal motion emacs)
+    "j"   'pdf-view-next-line-or-next-page
+    "k"   'pdf-view-previous-line-or-previous-page
+    "h"   'image-forward-hscroll
+    "l"   'image-backward-hscroll
+    "G"   'pdf-view-goto-page
+    "f"   'pdf-view-next-page
+    "b"   'pdf-view-previous-page
+    "d"   'pdf-view-scroll-up-or-next-page
+    "u"   'pdf-view-scroll-down-or-previous-page
+    (kbd "SPC") 'pdf-view-scroll-up-or-next-page
+    (kbd "DEL") 'pdf-view-scroll-down-or-previous-page
+    "o"   'pdf-outline
+    "+"   'pdf-view-enlarge
+    "-"   'pdf-view-shrink
+    "0"   'pdf-view-scale-reset
+    "q"   'quit-window))
+
+(pdf-tools-install)
