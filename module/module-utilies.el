76166da4b3077d41c24583e2edb77cd7ddd8f489
hongwozai <hongwozai@163.com>
2018-06-24 04:38:25 +0000

    加入测试的snippets

--- module/module-utilies.el
+++ module/module-utilies.el
@@ -6,3 +6,28 @@
   (save-restriction
    (narrow-to-defun)
    (swiper)))
+
+;;; gtest snippet
+(defvar snippets-gtest-assert-word
+      '("EQ"
+        "NE"
+        "LT"
+        "GT"
+        "LE"
+        "GE"
+        "TRUE"
+        "FALSE"
+        "STREQ"
+        "STRNE"
+        "STRCASEEQ"
+        "STRCASENE"
+        "THROW"
+        "ANY_THROW"
+        "NO_THROW"
+        "FLOAT_EQ"
+        "DOUBLE_EQ"
+        "DEATH"
+        "EXIT"))
+
+(defun yasnippets-gtest-assert-word ()
+  (ivy-read "Method: " snippets-gtest-assert-word))
