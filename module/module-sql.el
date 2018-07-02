6b306e816d894fc59c24031072cf1aa3c7fd7469
zeya <hongwozai@163.com>
2018-07-02 16:36:19 -0700

    添加module-sql

---
+++ module/module-sql.el
@@ -0,0 +1,7 @@
+;;-------------------------------------------
+;;; sql
+;;-------------------------------------------
+(require-package 'sql-indent)
+
+(with-eval-after-load "sql"
+  (load-library "sql-indent"))
