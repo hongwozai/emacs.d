860697c989fa8a4b002c63d7637baad57363ffa0
zeya <hongwozai@163.com>
2018-11-30 03:05:53 -0800

    powerline性能低下，从此不适用powerline

--- core/core-ui.el
+++ core/core-ui.el
@@ -53,6 +53,31 @@
 (add-hook 'after-change-major-mode-hook 'purge-minor-modes)
 
 ;;-------------------------------------------
+;;; don't use powerline, because too slow
+;;-------------------------------------------
+;;; mode-line
+(setq-default mode-line-format
+              '("%e"
+                " "
+                (:eval (winum-get-number-string))
+                mode-line-front-space
+                mode-line-mule-info
+                mode-line-client
+                mode-line-modified
+                mode-line-remote
+                ;; vim state
+                (:eval evil-mode-line-tag)
+                ;; line and column
+                "[" "%02l" "," "%01c" "] "
+                mode-line-frame-identification
+                mode-line-buffer-identification
+                (vc-mode vc-mode)
+                "  "
+                mode-line-modes
+                mode-line-misc-info
+                mode-line-end-spaces))
+
+;;-------------------------------------------
 ;;; startup mode
 ;;-------------------------------------------
 (setq initial-scratch-message ";;; Scratch\n\n\n")
