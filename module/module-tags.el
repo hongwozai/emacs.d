b97bf0d1036e794dfdec7d5d465b805c6655bee1
zeya <zeyalu@tencent.com>
2019-01-20 14:06:18 +0800

    增加gxref对projectile的支持

--- module/module-tags.el
+++ module/module-tags.el
@@ -5,6 +5,16 @@
 (require 'gxref)
 
 ;;-------------------------------------------
+;;; projectile support
+;;-------------------------------------------
+(advice-add 'projectile-regenerate-tags
+            :around
+            (lambda (orig-fun &rest args)
+              (let* ((isctags (yes-or-no-p "Use Ctags Backend?"))
+                     (gxref-gtags-label (if isctags "ctags" "")))
+                (gxref-create-db (projectile-project-root)))))
+
+;;-------------------------------------------
 ;;; xref config
 ;;-------------------------------------------
 (add-to-list 'xref-backend-functions 'gxref-xref-backend)
