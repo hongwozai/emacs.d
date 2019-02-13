77fcfb6e8d55e7855285f538c7ed195f63690a1d
zeya <zeyalu@tencent.com>
2019-02-13 11:17:49 +0800

    1. 不使用flymake 2. save coding system设置为utf-8

--- core/core-lsp.el
+++ core/core-lsp.el
@@ -16,6 +16,9 @@
 ;;; hook
 (add-hook 'lsp-after-open-hook #'lsp-enable-imenu)
 
+;;; don't use flymake
+(setq lsp-prefer-flymake nil)
+
 ;;; drop lsp-hover
 ;; (setq lsp-eldoc-hook '(lsp-document-highlight lsp-hover))
 (provide 'core-lsp)
