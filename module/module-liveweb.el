eac3d980411a651caf14213390014e04f21eb4f1
zeya <zeyalu@tencent.com>
2019-03-31 20:14:30 +0800

    去掉impatient-mode

--- module/module-liveweb.el
+++ module/module-liveweb.el
@@ -4,10 +4,8 @@
 (module-require-manual)
 (require-package 'simple-httpd)
 (require-package 'skewer-mode)
-(require-package 'impatient-mode)
 
 (setq httpd-root "~/www/")
-(setq httpd-port 8080)
 (setq httpd-host "0.0.0.0")
 
 (skewer-setup)
