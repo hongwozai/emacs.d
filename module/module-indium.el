02aa5c2a77a3f617f6e3cfe0eedd7e4c99ba7549
zeya <zeyalu@tencent.com>
2019-03-31 18:21:18 +0800

    试用indium

---
+++ module/module-indium.el
@@ -0,0 +1,18 @@
+;;-------------------------------------------
+;;; js develop environment
+;;-------------------------------------------
+;; (module-require-manual)
+(require-package 'indium)
+
+(autoload 'indium-interaction-mode "indium" "indium" t)
+
+(add-hook 'js-mode-hook 'indium-interaction-mode)
+(add-hook 'js2-mode-hook 'indium-interaction-mode)
+
+(evil-set-initial-state 'indium-repl-mode 'emacs)
+
+(core/set-key indium-repl-mode-map
+  :state 'emacs
+  (kbd "C-p") #'indium-repl-previous-input
+  (kbd "C-n") #'indium-repl-next-input
+  (kbd "C-u") #'kill-whole-line)
