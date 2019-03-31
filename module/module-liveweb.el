0450269f51dade153ea389430320addbeea1de0a
zeya <zeyalu@tencent.com>
2019-03-31 22:17:17 +0800

    避免skewer company不全时的bug

--- module/module-liveweb.el
+++ module/module-liveweb.el
@@ -8,3 +8,17 @@
 (setq httpd-host "0.0.0.0")
 
 (skewer-setup)
+
+;;-------------------------------------------
+;;; modify
+;;-------------------------------------------
+(defun skewer-repl-company-prefix ()
+  "Prefix for company."
+  (or (with-no-warnings ;; opportunistic use of company-mode
+        (company-grab-symbol-cons "\\." 1))
+      'stop))
+
+(add-hook 'skewer-repl-mode-hook
+          (lambda ()
+            (setq-local company-idle-delay nil)
+            (local-set-key (kbd "C-M-i") 'company-skewer-repl)))
