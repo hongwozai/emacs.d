32dc379ae62df2f096c576091304ea3225516f32
zeya <zeyalu@tencent.com>
2019-04-01 16:07:28 +0800

    js2-mode toggle 不如直接使用hs

--- module/module-js.el
+++ module/module-js.el
@@ -13,11 +13,3 @@
       js2-idle-timer-delay     0.1
       js2-indent-on-enter-key  t
       js2-auto-indent-p        t)
-
-(add-hook 'js2-mode-hook
-          (lambda ()
-            (core/set-key js2-mode-map
-              :state 'normal
-              (kbd "za") 'js2-mode-toggle-element
-              (kbd "zc") 'js2-mode-hide-element
-              (kbd "zo") 'js2-mode-show-element)))
