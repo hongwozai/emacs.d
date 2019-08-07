5583628c713cea3d4b8050a4ee4a8288e4c9800e
hongwozai <hongwozai@163.com>
2019-08-08 02:15:41 +1000

    java scala

--- module/module-scala.el
+++ module/module-scala.el
@@ -2,6 +2,13 @@
 ;;; package
 ;;-------------------------------------------
 (require-package 'ensime)
+(require-package 'scala-mode)
+(require-package 'sbt-mode)
 
 (setq ensime-sbt-command "sbt"
       sbt:program-name "sbt")
+
+(setq ensime-startup-notification nil)
+
+(setq ensime-search-interface 'ivy)
+(setq ensime-eldoc-hints 'all)
