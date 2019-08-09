8780cc8364f80a054d3de754d114da96b08410ec
zeya <zeyalu@tencent.com>
2019-08-09 21:09:55 +0800

    添加scala相关快捷键

--- module/module-scala.el
+++ module/module-scala.el
@@ -12,3 +12,9 @@
 
 (setq ensime-search-interface 'ivy)
 (setq ensime-eldoc-hints 'all)
+
+(core/set-key scala-mode-map
+  :state '(normal emacs)
+  (kbd "C-]") 'ensime-edit-definition
+  (kbd "M-.") 'ensime-edit-definition
+  (kbd "C-t") 'ensime-pop-find-definition-stack)
