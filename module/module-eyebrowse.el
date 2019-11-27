f78f06fad650c225fa2fe2333630553d23aff840
zeyalu <zeyalu@tencent.com>
2019-11-27 10:30:56 +0800

    eyebrowse插件

---
+++ module/module-eyebrowse.el
@@ -0,0 +1,8 @@
+;;-------------------------------------------
+;;; eyebrowse mode
+;;-------------------------------------------
+(require-package 'eyebrowse)
+
+(eyebrowse-setup-evil-keys)
+
+(eyebrowse-mode)
