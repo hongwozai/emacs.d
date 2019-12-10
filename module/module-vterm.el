5925a1a04ea35e48bbba27246c2261eefc854e51
zeyalu <zeyalu@tencent.com>
2019-12-10 09:42:32 +0800

    vterm快捷键

--- module/module-vterm.el
+++ module/module-vterm.el
@@ -11,3 +11,9 @@
             (define-key vterm-mode-map (kbd "C-u") 'vterm--self-insert)
             (core/auto-exit)
             ))
+
+(defalias 'vt 'vterm)
+
+(unless (assoc "find-file-other-window" vterm-eval-cmds)
+  (add-to-list 'vterm-eval-cmds
+              '("find-file-other-window" find-file-other-window)))
