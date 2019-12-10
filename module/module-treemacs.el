1daa4deb08a450c8b31a19919f6efe57a8794e6f
zeyalu <zeyalu@tencent.com>
2019-12-10 15:02:02 +0800

    treemacs配置

---
+++ module/module-treemacs.el
@@ -0,0 +1,8 @@
+;;-------------------------------------------
+;;; treemacs mode
+;;-------------------------------------------
+(module-require-manual)
+
+(require-package 'treemacs)
+(require-package 'treemacs-evil)
+(require-package 'treemacs-projectile)
