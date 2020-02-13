44c6d1c7826b0cd7d1cdf4d1290077e876b4c00c
zeyalu <zeyalu@tencent.com>
2020-02-13 17:08:25 +1000

    feat(markdown) 更新markdown配置，增强预览效果相关

--- module/module-markdown.el
+++ module/module-markdown.el
@@ -16,5 +16,58 @@
          ("README\\.txt\\'" . markdown-mode))
        auto-mode-alist))
 
-;;; C-c C-c l on-the-fly preview
+;;; C-c C-c l simple on-the-fly preview
 ;;; C-c C-s C/q/
+
+;;-------------------------------------------
+;;; livedown on-the-fly preview
+;;-------------------------------------------
+;;; npm install -g livedown
+;;; livedown start Readme.md --port 6419
+
+;;-------------------------------------------
+;;; configure
+;;-------------------------------------------
+(setq markdown-enable-wiki-links t
+      markdown-italic-underscore t
+      markdown-asymmetric-header t
+      markdown-make-gfm-checkboxes-buttons t
+      markdown-gfm-uppercase-checkbox t
+      markdown-fontify-code-blocks-natively t
+      markdown-enable-math t
+
+      markdown-content-type "application/xhtml+xml"
+      markdown-css-paths '("https://cdn.jsdelivr.net/npm/github-markdown-css/github-markdown.min.css"
+                           "https://cdn.jsdelivr.net/gh/highlightjs/cdn-release/build/styles/github.min.css")
+      markdown-xhtml-header-content "
+<meta name='viewport' content='width=device-width, initial-scale=1, shrink-to-fit=no'>
+<style>
+body {
+  box-sizing: border-box;
+  max-width: 740px;
+  width: 100%;
+  margin: 40px auto;
+  padding: 0 10px;
+}
+</style>
+<script src='https://cdn.jsdelivr.net/gh/highlightjs/cdn-release/build/highlight.min.js'></script>
+<script>
+document.addEventListener('DOMContentLoaded', () => {
+  document.body.classList.add('markdown-body');
+  document.querySelectorAll('pre[lang] > code').forEach((code) => {
+    code.classList.add(code.parentElement.lang);
+    hljs.highlightBlock(code);
+  });
+});
+</script>
+")
+
+
+;;-------------------------------------------
+;;; toc
+;;-------------------------------------------
+(require-package 'markdown-toc)
+
+(with-eval-after-load 'markdown-mode
+  (define-key markdown-mode-command-map
+    (kbd "r") 'markdown-toc-generate-or-refresh-toc))
