ae86a45f4449aba648885344d171dce7230a5838
zeya lu <zeyalu@Mac.lan>
2025-03-30 23:08:09 +0800

    fix: update py-activate and remove anoise output.

--- site-lisp/python-config.el
+++ site-lisp/python-config.el
@@ -40,8 +40,10 @@
     (setenv "PATH" (concat venv-bin path-separator (getenv "PATH")))
     ;; modify finished
     (py--update-mode-line)
-    (when (eglot-current-server)
-      (call-interactively #'eglot-reconnect))
+    ;; start lsp server
+    (if (eglot-current-server)
+        (call-interactively #'eglot-reconnect)
+      (eglot-ensure))
     (message "python venv %s activate already" py--current-venv-name)))
 
 (defun py--deactivate-venv ()
@@ -71,6 +73,15 @@
     ;; then
     venv-path))
 
+(defun py--find-interpreter (venv-path)
+  ;; 如果能在本venv找到ipython，那么才使用ipython
+  (when-let* ((dir (file-name-as-directory
+                    (concat (file-name-as-directory venv-path) "bin")))
+              (ipy (executable-find (concat dir "ipython"))))
+    ;; shell interpreter
+    (setq-local python-shell-interpreter "ipython")
+    (setq-local python-shell-interpreter-args "-i --simple-prompt")))
+
 ;;;###autoload
 (defun py-activate (venv-path)
   (interactive
@@ -82,7 +93,9 @@
            (directory-file-name
             (read-directory-name "Read VEnv Directory:")))))
   (if (file-exists-p venv-path)
-      (py--activate-venv venv-path)
+      (progn
+        (py--activate-venv venv-path)
+        (py--find-interpreter venv-path))
     (message "no venv %s" venv-path)))
 
 ;;;###autoload
@@ -90,26 +103,38 @@
   (interactive)
   (py--deactivate-venv))
 
-(defun py-mode-hook-function ()
-  ;; find python shell interpreter
-  (let* ((map (symbol-value (intern (format "%s-map" major-mode))))
-         (venv-path py--current-venv-path)
-         (venv-dir (when venv-path
-                     (file-name-as-directory
-                      (concat (file-name-as-directory venv-path)
-                              "bin")))))
-
-    ;; 如果能在本venv找到ipython，那么才使用ipython
-    (when-let* ((dir venv-dir)
-                (ipy (executable-find (concat dir "ipython"))))
-      ;; shell interpreter
-      (setq-local python-shell-interpreter "ipython")
-      (setq-local python-shell-interpreter-args "-i --simple-prompt"))))
-
 ;; first, we update modeline
 (py--update-mode-line)
 
+;;-------------------------------------------
+;;; filter
+;;-------------------------------------------
+(defun py--comint-filter (output)
+  (replace-regexp-in-string "__PYTHON_EL_eval.+\n" "" output))
+
+(defun py--eldoc-filter (orig-fun string)
+  (let ((clean
+         (if (stringp string)
+             (replace-regexp-in-string "\n__PYTHON_EL_eval.+" "" string)
+           string)))
+    (funcall orig-fun clean)))
+
+;;;###autoload
+(defun py-mode-hook-function ()
+  ;; find python shell interpreter
+  (let* ((map (symbol-value (intern (format "%s-map" major-mode))))
+         (venv-path py--current-venv-path))
+    ;; switch interpreter
+    (py--find-interpreter venv-path)))
+
 (add-hook 'python-mode-hook 'py-mode-hook-function)
 (add-hook 'python-ts-mode-hook 'py-mode-hook-function)
+
+(when *is-mac*
+  (with-eval-after-load 'comint
+    (add-hook 'comint-preoutput-filter-functions #'py--comint-filter nil t))
+
+  (with-eval-after-load 'eldoc
+    (advice-add 'eldoc--message :around #'py--eldoc-filter)))
 
 (provide 'python-config)
